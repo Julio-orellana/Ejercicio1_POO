978418afcd0fb28face05eb304246d1c6b5f796c
Julio <julioes134@outlook.es>
2024-07-30 15:49:38 -0600

    Constructors and getLocation method added

--- src/Stadium.java
+++ src/Stadium.java
@@ -1,3 +1,31 @@
 public class Stadium {
 
+    private String name;
+    private int totalCapacity;
+    private Location[] locations;
+    private int ticketsSold;
+    private double revenue;
+
+    public Stadium(String name, int totalCapacity, Location[] locations, int tkSold, double revenue){
+        this.name = name;
+        this.totalCapacity = totalCapacity;
+        this.locations = locations;
+        this.ticketsSold = tkSold;
+        this.revenue = revenue;
+    }
+
+    public Stadium(String name, int totalCapacity, Location[] locations, int tkSold){
+        this.name = name;
+        this.totalCapacity = totalCapacity;
+        this.locations = locations;
+        this.ticketsSold = tkSold;
+    }
+
+    public Location getLocation(String locationName){
+        for (int i = 0; i < this.locations.length; i++){
+            if (this.locations[i].getName() == locationName) return this.locations[i];
+            continue;
+        }
+    }
+
 }

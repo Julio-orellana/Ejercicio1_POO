226ad1e7b06a951708102a0936049e4e4ce4e566
Julio <julioes134@outlook.es>
2024-07-30 20:57:15 -0600

    New objects instantiated for testing

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Location[] locations = {
+            new Location("Gradas", 5, 0, 300),
+            new Location("Vip", 5, 0, 1500),
+            new Location("Vip de pie", 5, 0, 800),
+            new Location("General", 5, 0, 500)
+        };
+
+        Stadium stadium = new Stadium("Bernabeu",20,locations, 0, 0);
+
+        //client.buyTickets(stadium, location, 3, 1500);
+        //client.buyTickets(stadium, location, oldTickets, 3, 1500);
     }
 }
